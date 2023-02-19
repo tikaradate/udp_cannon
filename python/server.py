0af7cf60be6c8c32ce63b67d956b027206cc0dbc
tikaradate <tikaradate@gmail.com>
2023-02-19 15:27:22 -0300

    Inicia log python

--- python/server.py
+++ python/server.py
@@ -1,5 +1,12 @@
 import socket
 import sys
+from enum import Enum
+MAXMESSAGES = 100000
+class Type(Enum):
+    NOTRECV = 0
+    INORDER = -1
+    OUTOFORDER = -2
+
 if len(sys.argv) != 2:
     print("Uso: python server.py <porta>")
     exit(1)
@@ -24,16 +31,44 @@ server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 server_socket.bind((host, port))
 
+num_seqs = [Type.NOTRECV]*MAXMESSAGES
+expected_seq = 0
 server_socket.settimeout(1)
-received = []
-while(True):
+while(True != False):
     try:
         bytesAddressPair = server_socket.recvfrom(bufferSize)
         message = bytesAddressPair[0]
-        received.append(message)
+        recv_seq = int(message.decode('utf-8'))
+        if(expected_seq != recv_seq):
+            if(recv_seq < expected_seq):
+                num_seqs[recv_seq] = Type.OUTOFORDER
+            else:
+                num_seqs[recv_seq] = recv_seq - expected_seq
+                expected_seq = recv_seq + 1
+        else:
+            num_seqs[recv_seq] = Type.INORDER
+            expected_seq += 1
+
     except socket.timeout:
         print("timeout")
         break
 
-for message in received:
-    print(message)
+ooo = 0
+lost = 0
+ok = 0
+
+for i in range(expected_seq):
+    if(num_seqs[i] != Type.INORDER):
+        if(num_seqs[i] == Type.OUTOFORDER):
+            ooo += 1
+        elif(num_seqs[i] == Type.NOTRECV):
+            lost += 1
+        else:
+            ooo += 1
+    else:
+        ok += 1
+
+print("Total: %d" % (expected_seq))
+print("OK: %d" % (ok))
+print("Out of order: %d" % (ooo))
+print("Lost: %d" % (lost))
